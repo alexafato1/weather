2ebf72036823137a4f5d4c57bc6a4f0fb00bfc99
Alexa fato <alexafato@MacBook-Air-Alexa.local>
2020-06-25 18:14:54 +0300

    2

--- public/script.js
+++ public/script.js
@@ -1,21 +1,22 @@
 const searchElement = document.querySelector('[data-city-search]')
 const searchBox = new google.maps.places.SearchBox(searchElement)
-SearchBox.addListener('places_changed', () => {
-    const place = search.getPlaces()[0]
-    if(place== null) return
-    const latitude = place.geometry.location.lat()
-    const longitude = place.geometry.location.ing()
-    fetch('/weather',{
-       method:'POST',
-       headers: {
-           'Content-Type': 'application/json',
-           'Accept': 'application/json'
-       },
-       body: JSON.stringify({
-           latitude: latitude,
-           longitude: longitude
-       })
-    }).then(res => res.json()).then(data => {
-        setWeatherData(data, place.formatted_address)
+searchBox.addListener('places_changed', () => {
+  const place = searchBox.getPlaces()[0]
+  if (place == null) return
+  const lat = place.geometry.location.lat()
+  const lon = place.geometry.location.lng()
+  fetch('/weather', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      lat: lat,
+      lon: lon
     })
+  }).then(res => res.json()).then(data => {
+    console.log(data)
+   // setWeatherData(data, place.formatted_address)
+  })
 })
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-if(process.env.NODE_ENV !== 'productio'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
- const API =
- process.env.API
- const express = require('express')
- const app = express()
- app.use(express.json())
- app.use(express.static('public'))
 
- app.post('/weather', (req, res) => {
- console.log(req.body)
- })
+const API = process.env.API
+const axios = require('axios')
+const express = require('express')
+const app = express()
 
- app.listen(3000, () => {
-   console.log('Server started') 
- })
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/weather', (req, res) => {
+  console.log(req.body)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.body.lat}&lon=${req.body.lon}&appid=${API}`
+  axios({
+    url: url,
+    
+    responseType: 'json'
+  }).then(data => res.json(data.data))
+  console.log(req.body)
+  console.log(url)
+  
+})
+
+app.listen(3000, () => {
+  console.log('Server Started')
+})
